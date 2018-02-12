af1096d98e84e16be493739e9cb9a62ec3070e0d
Griswald Brooks <griswald.brooks@gmail.com>
2018-02-12 09:30:47 -0800

    Added try and solve.

--- taxi.py
+++ taxi.py
@@ -8,13 +8,24 @@ env = gym.make('Taxi-v2')
 Q = np.zeros([env.observation_space.n, env.action_space.n])
 alpha = 0.618
 
-for episode in range(1, 1001):
-    done = False
-    G, reward = 0, 0
-    state = env.reset()
-    while not done:
-        act = np.argmax(Q[state])
-        state_, reward, done, info = env.step(act)
-        Q[state, act] += alpha * (reward + np.max(Q[state_]) - Q[state, act])
-        G += reward
-        state = state_
+try:
+    for episode in range(1, 1001):
+        done = False
+        G, reward = 0, 0
+        s = env.reset()
+        while not done:
+            act = np.argmax(Q[s])
+            s_, reward, done, info = env.step(act)
+            Q[s, act] += alpha * (reward + np.max(Q[s_]) - Q[s, act])
+            G += reward
+            s = s_
+            env.render()
+except Exception as e:
+    print('oops')
+
+done = False
+state = env.reset()
+while not done:
+    act = np.argmax(Q[state])
+    state, r, done, i = env.step(act)
+    env.render()
